f14b87431a38c5dcc813d528a876020d4fff1af4
Prabhakar Pal <prabhakar.pal00@gmail.com>
2024-03-08 23:49:00 -0500

    formatted text

--- terrible-database/includes/tokenizer.h
+++ terrible-database/includes/tokenizer.h
@@ -1,60 +1,63 @@
 #ifndef TOKENS_H
 #define TOKENS_H
 
-#include <string>
 #include <sstream>
+#include <string>
 #include <unordered_map>
-#include <vector>
 #include <utility>
+#include <vector>
 
 namespace tdb {
-    enum Token {
-        TEXT,
-        CREATE,
-        INSERT,
-        DELETE,
-        UPDATE,
-        SELECT,
-        WHERE,
-        FROM,
-        OP_EQ, // ==
-        OP_GT, // >
-        OP_LT, // <
-        OP_EQGT, // >=
-        OP_EQLT, // <=
-        AND,
-        OR,
-        TYPE_INT,
-        TYPE_DOUBLE,
-        TYPE_STRING,
-        VALUES,
-        WITH,
-        END
-    };
+// clang-format off
+enum Token {
+    TEXT,
+    CREATE,
+    INSERT,
+    DELETE,
+    UPDATE,
+    SELECT,
+    WHERE,
+    FROM,
+    OP_EQ, // ==
+    OP_GT, // >
+    OP_LT, // <
+    OP_EQGT, // >=
+    OP_EQLT, // <=
+    AND,
+    OR,
+    TYPE_INT,
+    TYPE_DOUBLE,
+    TYPE_STRING,
+    VALUES,
+    WITH,
+    END
+};
 
-    std::vector<std::pair<Token, std::string>> ReadInputQuery(std::string input_query);
+const std::unordered_map<std::string, Token> token_lookup = {
+    { "create", CREATE },
+    { "insert", INSERT },
+    { "update", UPDATE },
+    { "delete", DELETE },
+    { "select", SELECT },
+    { "where", WHERE },
+    { "from", FROM },
+    { "==", OP_EQ },
+    { ">", OP_GT },
+    { "<", OP_LT },
+    { ">=", OP_EQGT },
+    { "<=", OP_EQLT },
+    { "and", AND },
+    { "or", OR },
+    { "int", TYPE_INT },
+    { "double", TYPE_DOUBLE},
+    { "string", TYPE_STRING},
+    { "values", VALUES },
+    { "with", WITH }
+};
+// clang-format on
 
-    const std::unordered_map<std::string, Token> token_lookup = {
-        { "create", CREATE },
-        { "insert", INSERT },
-        { "update", UPDATE },
-        { "delete", DELETE },
-        { "select", SELECT },
-        { "where", WHERE },
-        { "from", FROM },
-        { "==", OP_EQ },
-        { ">", OP_GT },
-        { "<", OP_LT },
-        { ">=", OP_EQGT },
-        { "<=", OP_EQLT },
-        { "and", AND },
-        { "or", OR },
-        { "int", TYPE_INT },
-        { "double", TYPE_DOUBLE},
-        { "string", TYPE_STRING},
-        { "values", VALUES },
-        { "with", WITH }
-    };
-}
+std::vector<std::pair<Token, std::string>>
+ReadInputQuery(std::string input_query);
+} // namespace tdb
 
 #endif
