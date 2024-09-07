b100f335d7f0df3495b43186ea4ec227b67ef963
Prabhakar Pal <prabhakar.pal00@gmail.com>
2024-09-07 21:10:11 +0530

    fixed register size of 32

--- includes/interpreter.h
+++ includes/interpreter.h
@@ -1,28 +1,25 @@
 #pragma once
 
-#include <vector>
-
 #include "instructions.h"
 #include "symbol_table.h"
 
 namespace sci {
+  
+static constexpr unsigned int reg_size = 32;
+
 class Interpreter {
  public:
-  Interpreter(SymbolTable &st, size_t reg_size = 1000)
-      : m_registers(reg_size), m_st(st) {}
+  Interpreter(SymbolTable &st)
+      : m_st(st) {}
 
   void Interpret(std::vector<Instruction> instructions);
 
  private:
   // 0 idx fixed for rax (storing function return value)
-  std::vector<double> m_registers;
+  double m_registers[reg_size];
   SymbolTable &m_st;
 
   inline void UpdateReg(size_t idx, double val) {
-    [[unlikely]] if (idx > m_registers.size()) {
-      m_registers.resize(m_registers.size() * 0.5);
-    }
-
     m_registers[idx] = val;
   }
 
